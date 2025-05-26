f55e41cc6e52ffecbf828dce3c1f984a989fe7ff
jstgrowup <deysubham999@gmail.com>
2025-05-26 22:32:06 +0530

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,26 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+"use client";
+import PersonaReact from "persona-react";
 export default function Home() {
+  console.log(
+    "process.env.NEXT_PUBLIC_PERSONA_TEMPLATE:",
+    process.env.NEXT_PUBLIC_PERSONA_TEMPLATE
+  );
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      <PersonaReact
+        templateId={process.env.NEXT_PUBLIC_PERSONA_TEMPLATE}
+        environmentId={process.env.NEXT_PUBLIC_PERSONA_ENVIROMENT_ID}
+        // This is for the webhook
+        referenceId={"abcd"}
+        onComplete={({ inquiryId, status }) => {
+          console.log("status:", status);
+          console.log("inquiryId:", inquiryId);
+          // TODO: This will be handled in the webhook and will remove this hook call
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          if (status === "completed") {
+          }
+        }}
+      />
     </div>
   );
 }
